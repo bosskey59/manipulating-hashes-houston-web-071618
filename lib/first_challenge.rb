b552aa32c0a7aa27abf22a0a4687e5792392257e
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-07-06 14:12:46 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   contacts["Freddy Mercury"].each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.each do |flavors|
-        flavors.dele
+        flavors.delete_if("strawberry")
       end
     end
     
